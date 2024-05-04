38ad9c03b39ab995342f73139d8a0eb1c2cddcd0
Batouuuuu <gillet.baptiste94@gmail.com>
2024-05-04 18:25:00 +0200

    feat: détection de la langue et prétraitement du commentaire

--- scripts/extraction.py
+++ scripts/extraction.py
@@ -3,7 +3,7 @@ Ce script récuppère les commentaires positifs et négatif du jeu skyrim, const
 commentaire et ecrit ces commentaires dans un csv.
 """
 
-
+import re 
 import csv
 from pathlib import Path
 from bs4 import BeautifulSoup 
@@ -11,33 +11,18 @@ from langdetect import detect
 from datascturtures import Commentaire
 from typing import List
 
-    ##questions de prétraitement :
-    ## - les qcm
-    ## les emojis 
-    ## les images avec dessinées avec des ___
-    ## ne prendre en compte que les commentaires avec un certains nombre de mots : FAIT
-    ## certains coms restent en anglais
 
-def traitement_des_langues(chemin_resultat : str | Path) -> bool:
 
-    # print(len(chemin_resultat))
-    chemin_resultat = Path(chemin_resultat)
-
-    for file in chemin_resultat.glob("*.txt"):
-        with open(file, "r") as f:
-            content = f.read()
-            langue = detect(content)
-            if langue == 'fr':
-                print(f"{file}\t{detect(content)}")
-                return True
-            else:
-                print(f"On a skip cette langue : {langue}")
-                return False
-
+def commentaire_français(texte : str | Path) -> bool:
+    """Vérifie si un texte est en français."""
+    if len(texte) < 9:  ## on prédéfinit que notre texte doit avoir plus de 9 caractères
+        return False
+    langue = detect(texte)
+    return langue == 'fr'
 
 def pretraitement(page_html: str | Path) -> str:
+    """Fonction qui permet de mieux supprimer le tag de la date et aussi prétraiter le texte"""
 
-    ## prétraitement : 
     ## il faut supprimer le tag date_posted car le résultat est moche et on ne le veut pas, il est contenu dans la balise qui contient le commentaire
 
     with open(page_html, 'r' ) as f:
@@ -45,40 +30,53 @@ def pretraitement(page_html: str | Path) -> str:
 
     for tag in soup.find_all("div", class_="date_posted"):
         tag.decompose()
+
+    ## pour enlever les tabulations
+    for tag in soup.find_all(attrs={'class':'apphub_CardTextContent'}):
+        tag.string = tag.get_text(strip=True)
+
+    ## utilisation d'une expression régulière afin de retirer les commentaire qui sont des dessins graphiques
+    for tag in soup.find_all(attrs={'class':'apphub_CardTextContent'}):
+        cleaned_text = re.sub(r'[^a-zA-Z\s-]', '', tag.get_text()) 
+        tag.string = cleaned_text.lower()  
+    
     return soup
 
 
 def chargement_commentaire_positif(soup : BeautifulSoup, chemin_resultat : str | Path) -> List[Commentaire]:
-
+    """Cette fonction créee un fichier par commentaire positif, elle vérifie que le commentaire est français
+    et """
     # surement pas la meilleure technique j'avais des problemes pour manipuler le tag cela m'indiquait que c'était un tag et non un str du coup j'ai fait une liste
 
     tag = soup.find_all(attrs={'class':'apphub_CardTextContent'})
     liste = []
     liste_objet_positif = []
     i = 0
+
     for element in tag:
         texte = element.get_text()
         texte = texte.lower()
-        mots = texte.split()
-        if len(mots) >= 10: ## il doit y avoir plus de 10 mots dans le commentaire pour être pris en compte
-            liste.append(element.get_text()) ## ajout à la premiere liste
-            commentaire_positif = Commentaire(id_fichier=i, sentiment="positif", commentaire=texte) ## ajout à la liste de notre dataclass commentaire
-            liste_objet_positif.append(commentaire_positif)
-            i+=1
+        # print(texte)
+        if commentaire_français(texte):
+            mots = texte.split()
+
+            if len(mots) >= 10: ## il doit y avoir plus de 10 mots dans le commentaire pour être pris en compte
+                liste.append(element.get_text()) ## ajout à la premiere liste
+                commentaire_positif = Commentaire(id_fichier=i, sentiment="positif", commentaire=texte) ## ajout à la liste de notre dataclass commentaire
+                liste_objet_positif.append(commentaire_positif)
+                i+=1
     print(f'{len(liste)}')
+    
     try:
         for i, element in enumerate(liste): 
             element = element.lower()
             with open(f"{chemin_resultat}/commentaire_positif_{i}.txt", 'w', encoding='UTF8') as resultat:
                 resultat.write(element.strip())
-                # if not traitement_des_langues(resultat.name):
-                #     # on supprime le fichier si c'est pas en français
-                #     resultat.close()
-                #     Path(resultat.name).unlink()
+
     except Exception as e:
         print(f"Il y'a eu une erreur : {e}")
     
-    return liste ,liste_objet_positif
+    return liste_objet_positif
 
 def chargement_commentaire_negatif(soup : BeautifulSoup, chemin_resultat : str | Path) -> List[Commentaire]:
     """Récuppération des commentaires négatif, même principe que la fonction pour les commentaires positifs"""
@@ -87,15 +85,21 @@ def chargement_commentaire_negatif(soup : BeautifulSoup, chemin_resultat : str |
     liste = []
     liste_objet_negatif = []
     i = 0
+    
     for element in tag:
         texte = element.get_text()
-        mots = texte.split()
-        if len(mots) >= 10: 
-            liste.append(element.get_text())
-            commentaire_negatif = Commentaire(id_fichier=i, sentiment="negatif", commentaire=texte, )
-            liste_objet_negatif.append(commentaire_negatif)
-            i+= 1
+        if commentaire_français(texte):
+            mots = texte.split()
+        
+            if len(mots) >= 10: 
+                liste.append(element.get_text())
+                commentaire_negatif = Commentaire(id_fichier=i, sentiment="negatif", commentaire=texte, )
+                liste_objet_negatif.append(commentaire_negatif)
+                i+= 1
+                if i >= 244: ## on récupère 243 commentaires négatifs afin d'avoir le même nombre de commentaires négatifs et positifs
+                    break
     print(f'{len(liste)}')
+    
     try:
         for i, element in enumerate(liste):
             element = element.lower() # on met tout en minuscule pour éviter les erreurs avec langdetect
@@ -107,6 +111,8 @@ def chargement_commentaire_negatif(soup : BeautifulSoup, chemin_resultat : str |
     return liste_objet_negatif
 
 def ecrire_commentaire_csv(liste_commentaires, nom_fichier):
+    """ecriture des commentaires dans notre csv"""
+    
     with open(nom_fichier, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['id_fichier', 'sentiment', 'commentaire'])  # Écrire l'en-tête des colonnes
